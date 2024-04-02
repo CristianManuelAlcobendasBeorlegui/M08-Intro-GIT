ae923d09421d744596268f451dc5c763e2b6609d
Cristian Manuel Alcobendas Beorlegui <cristian.alcobendas@insbaixcamp.cat>
2024-04-02 19:12:10 +0200

    Se añade sección con un reloj que enseña la hora.

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 
 // === ELEMENTOS DEL DOM === //
 let spanFecha;
+let spanHora;
 
 // === EVENTOS === //
 window.addEventListener("load", init, false);
@@ -13,9 +14,11 @@ window.addEventListener("load", init, false);
 function init() {
     // Vincula las variables con los elementos del DOM
     spanFecha = document.getElementById("span-fecha");
+    spanHora = document.getElementById("span-hora");
 
     // Llama a algunos metodos
     mostrarFecha();
+    setInterval(mostrarHora, 1000);
 }
 
 /** 
@@ -27,4 +30,16 @@ function mostrarFecha() {
     let mes = date.getMonth().toString().padStart(2, 0);
     let anyo = date.getFullYear().toString().padStart(2, 0);
     spanFecha.innerHTML = dia + "/" + mes + "/" + anyo;
+}
+
+/** 
+ * Muestra la hora actual en formato HH:MM:SS.
+ * */
+function mostrarHora() {
+    let date = new Date();
+    let segundos = date.getSeconds().toString().padStart(2, 0);
+    let minutos = date.getMinutes().toString().padStart(2, 0);
+    let horas = date.getHours().toString().padStart(2, 0);
+
+    spanHora.innerHTML = horas + ":" + minutos + ":" + segundos;
 }
